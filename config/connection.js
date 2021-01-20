7c4af05812a3df7416a21b80f8d3bfb831452e29
Muhammad Anas <muhammadanas112@gmail.com>
2021-01-20 10:07:43 -0500

    need to add views folder and deploy

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,21 @@
-///
+/// define mysql connection with burger_db
+var mysql = mysql.createConnection({
+    host: "localhost",
+
+    port: 3010,
+
+    user: "root",
+
+    password: "",
+    database: "burger_db"
+});
+var connection;
+///error function
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+  });
+
+  ///Export module
+  module.exports = connection;
